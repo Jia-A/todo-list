16075c05cdbff2a0865683372e8e89457b05262e
Jia-A <agrawaljiya2011@gmail.com>
2021-12-17 00:52:38 +0530

    style:animation-added

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 var todoInput = document.querySelector(".todo-input");
 var todoBtn = document.querySelector(".todo-button");
 var todoOutput = document.querySelector(".todo-output");
+var filterOp =document.querySelector(".filter-todo");
 
 todoBtn.addEventListener("click", addTask);
 todoOutput.addEventListener("click", deleteCheck);
+filterOp.addEventListener("click", filterTodo)
 
 
 function addTask(event) {
@@ -35,11 +37,42 @@ function deleteCheck(event) {
     var item = event.target;
     if (item.classList[0] === "todo-trash") {
         var todo = item.parentElement;
-        todo.remove();
+
+        todo.classList.add("fall");
+        todo.addEventListener("transitionend", function(){
+            todo.remove();
+        })
+        
     }
 
     if (item.classList[0] === "todo-complete") {
         var todo = item.parentElement;
         todo.classList.toggle("completed");
     }
+}
+
+
+function filterTodo(event){
+    var todos = todoOutput.childNodes;
+    todos.forEach(function(todo){
+        switch(event.target.value){
+            case "all":
+                todo.style.display= "flex";
+                break;
+            case "completed":
+                if(todo.classList.contains("completed")){
+                    todo.style.display= "flex";
+                }
+                else{
+                    todo.style.display= "none"; 
+                }
+            case "incomplete":
+                if(todo.classList.contains("incomplete")){
+                    todo.style.display= "flex";
+                }
+                else{
+                    todo.style.display= "none"; 
+                }
+        }
+    });
 }
